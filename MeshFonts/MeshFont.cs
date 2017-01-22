04debbc2dbfd5b3cb6b2c634f8e71a114d14d2ed
Calix1994 <cali94xxx@gmail.com>
2017-01-21 22:50:11 -0500

    Add files via upload

---
+++ MeshFonts/MeshFont.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFont{
+
+    public string fontName;
+
+    #region Mesh Alpahbet
+    public GameObject mesh_A;
+    public float spaceBeforeA;
+    public float spaceAfterA;
+
+    public GameObject mesh_B;
+    public float spaceBeforeB;
+    public float spaceAfterB;
+
+    public GameObject mesh_C;
+    public float spaceBeforeC;
+    public float spaceAfterC;
+
+    public GameObject mesh_D;
+    public float spaceBeforeD;
+    public float spaceAfterD;
+
+    public GameObject mesh_E;
+    public float spaceBeforeE;
+    public float spaceAfterE;
+
+    public GameObject mesh_F;
+    public float spaceBeforeF;
+    public float spaceAfterF;
+
+    public GameObject mesh_G;
+    public float spaceBeforeG;
+    public float spaceAfterG;
+
+    public GameObject mesh_H;
+    public float spaceBeforeH;
+    public float spaceAfterH;
+
+    public GameObject mesh_I;
+    public float spaceBeforeI;
+    public float spaceAfterI;
+
+    public GameObject mesh_J;
+    public float spaceBeforeJ;
+    public float spaceAfterJ;
+
+    public GameObject mesh_K;
+    public float spaceBeforeK;
+    public float spaceAfterK;
+
+    public GameObject mesh_L;
+    public float spaceBeforeL;
+    public float spaceAfterL;
+
+    public GameObject mesh_M;
+    public float spaceBeforeM;
+    public float spaceAfterM;
+
+    public GameObject mesh_N;
+    public float spaceBeforeN;
+    public float spaceAfterN;
+
+    public GameObject mesh_O;
+    public float spaceBeforeO;
+    public float spaceAfterO;
+
+    public GameObject mesh_P;
+    public float spaceBeforeP;
+    public float spaceAfterP;
+
+    public GameObject mesh_Q;
+    public float spaceBeforeQ;
+    public float spaceAfterQ;
+
+    public GameObject mesh_R;
+    public float spaceBeforeR;
+    public float spaceAfterR;
+
+    public GameObject mesh_S;
+    public float spaceBeforeS;
+    public float spaceAfterS;
+
+    public GameObject mesh_T;
+    public float spaceBeforeT;
+    public float spaceAfterT;
+
+    public GameObject mesh_U;
+    public GameObject mesh_V;
+    public GameObject mesh_W;
+    public GameObject mesh_X;
+    public GameObject mesh_Y;
+    public GameObject mesh_Z;
+    #endregion
+
+}
---
+++ TextToMeshConverter.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextToMeshConverter{
+
+    public List<string> textList = new List<string>();
+    public MeshFont myFont;
+
+    void createMesh(string text)
+    {
+        createMesh(text, 0, 0, 0);
+    }
+
+
+    void createMesh(string text, float xPos, float yPos, float zPos)
+    {
+        // The list of GameObjects that will be Instantiated
+        List<GameObject> meshesToCreate = new List<GameObject>();
+
+        // Make sure that all text is Upper Case
+        string myText = text;
+        myText.ToUpper();
+
+        // Convert string to an array of characters, to make parsing easier
+        char[] myTextArray = myText.ToCharArray();
+
+        #region Populate meshesToCreate...
+        for (int i = 0; i >= myTextArray.Length; i++)
+        {
+
+            switch (myTextArray[i])
+            {
+                case 'A':
+                    {
+                        meshesToCreate.Add(myFont.mesh_A);
+                        break;
+                    }
+                case 'B':
+                    {
+                        meshesToCreate.Add(myFont.mesh_B);
+                        break;
+                    }
+                case 'C':
+                    {
+                        meshesToCreate.Add(myFont.mesh_C);
+                        break;
+                    }
+                case 'D':
+                    {
+                        meshesToCreate.Add(myFont.mesh_D);
+                        break;
+                    }
+                case 'E':
+                    {
+                        meshesToCreate.Add(myFont.mesh_E);
+                        break;
+                    }
+                case 'F':
+                    {
+                        meshesToCreate.Add(myFont.mesh_F);
+                        break;
+                    }
+                case 'G':
+                    {
+                        meshesToCreate.Add(myFont.mesh_G);
+                        break;
+                    }
+                case 'H':
+                    {
+                        meshesToCreate.Add(myFont.mesh_H);
+                        break;
+                    }
+                case 'I':
+                    {
+                        meshesToCreate.Add(myFont.mesh_I);
+                        break;
+                    }
+                case 'J':
+                    {
+                        meshesToCreate.Add(myFont.mesh_J);
+                        break;
+                    }
+                case 'K':
+                    {
+                        meshesToCreate.Add(myFont.mesh_K);
+                        break;
+                    }
+                case 'L':
+                    {
+                        meshesToCreate.Add(myFont.mesh_L);
+                        break;
+                    }
+                case 'M':
+                    {
+                        meshesToCreate.Add(myFont.mesh_M);
+                        break;
+                    }
+                case 'N':
+                    {
+                        meshesToCreate.Add(myFont.mesh_N);
+                        break;
+                    }
+                case 'O':
+                    {
+                        meshesToCreate.Add(myFont.mesh_O);
+                        break;
+                    }
+                case 'P':
+                    {
+                        meshesToCreate.Add(myFont.mesh_P);
+                        break;
+                    }
+                case 'Q':
+                    {
+                        meshesToCreate.Add(myFont.mesh_Q);
+                        break;
+                    }
+                case 'R':
+                    {
+                        meshesToCreate.Add(myFont.mesh_R);
+                        break;
+                    }
+                case 'S':
+                    {
+                        meshesToCreate.Add(myFont.mesh_S);
+                        break;
+                    }
+                case 'T':
+                    {
+                        meshesToCreate.Add(myFont.mesh_T);
+                        break;
+                    }
+                case 'U':
+                    {
+                        meshesToCreate.Add(myFont.mesh_U);
+                        break;
+                    }
+                case 'V':
+                    {
+                        meshesToCreate.Add(myFont.mesh_V);
+                        break;
+                    }
+                case 'W':
+                    {
+                        meshesToCreate.Add(myFont.mesh_W);
+                        break;
+                    }
+                case 'X':
+                    {
+                        meshesToCreate.Add(myFont.mesh_X);
+                        break;
+                    }
+                case 'Y':
+                    {
+                        meshesToCreate.Add(myFont.mesh_Y);
+                        break;
+                    }
+                case 'Z':
+                    {
+                        meshesToCreate.Add(myFont.mesh_Z);
+                        break;
+                    }
+                default:
+                    {
+                        Debug.LogWarning("Attempted to create mesh of unrecognized or unsupported character /'" + myTextArray[i] + "/'. Mesh has not been created.");
+                        break;
+                    }
+            }
+        }
+        #endregion
+                      
+    }
+} 
